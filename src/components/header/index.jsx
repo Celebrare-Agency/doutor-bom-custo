9169a89bc0b6356cb4a4e0d78328023463993827
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2023-10-02 10:21:53 -0300

    header compressed

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,121 +2,84 @@ import { HeaderContainer } from "./style";
 import logo from "../../assets/logoExtendBlue.svg";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { Link, useLocation } from "react-router-dom";
-import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
+import { Link as ScrollLink } from "react-scroll";
 import { useEffect, useState } from "react";
+const wppLink =
+  "https://api.whatsapp.com/send?phone=5511950212678&text=Olá!%20Eu%20gostaria%20de%20agendar%20uma%20consulta%20com%20Oftalmologista.%20";
+const menuItems = [
+  { path: "sobre", label: "Sobre nós" },
+  { path: "/oftalmo", label: "Serviços" },
+  { path: "duvidas", label: "Dúvidas" },
+  { path: wppLink, label: "Atendimento" },
+  { path: "parceiros", label: "Parceiros" },
+];
+
 export default function Header() {
   const [menuIsVisible, setMenuIsVisible] = useState(false);
+  const [animation, setAnimation] = useState(false);
   const path = useLocation().pathname;
+
   useEffect(() => {
-    function scrollUp() {
-      window.scroll(0, 0);
+    function handleScroll() {
+      setAnimation(window.scrollY > 2300);
     }
-    scrollUp();
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleMenuClose = () => {
+    setMenuIsVisible(false);
+  };
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    scrollToTop();
   }, [path]);
 
-  const [animation, setAnimation] = useState(false);
-
-  useEffect(() => {
-    const elementOnscroll = document.addEventListener("scroll", function () {
-      if (window.scrollY > 2300) {
-        setAnimation(true);
-      } else {
-        setAnimation(false);
-      }
-    });
-
-    document.addEventListener("DOMContentLoaded", function () {
-      elementOnscroll();
-    });
-  }, []);
-  useEffect(() => {
-    const elementOnscroll = document.addEventListener("scroll", function () {
-      if (window.scrollY > 100) {
-        setAnimation(true);
-      }
-    });
-
-    document.addEventListener("DOMContentLoaded", function () {
-      elementOnscroll();
-    });
-  }, []);
   return (
-    <>
-      <HeaderContainer isVisible={menuIsVisible} isanimation={animation}>
-        <nav className="header">
-          <div className="content">
-            <Link to="/">
-              <div className="logoSide">
-                <img src={logo} alt="logotipo" />
-              </div>
-            </Link>
-            <AiOutlineMenu
-              onClick={() => setMenuIsVisible(true)}
-              className="menu"
-            />
-            <AiOutlineClose
-              onClick={() => setMenuIsVisible(false)}
-              className="menu closeMenu"
-            />
-            <li className="menuSide">
-              <ScrollLink to="sobre">
-                <Link to="/">
-                  <ul
-                    className={`${
-                      path === "sobre" ? " color Options" : "Options"
-                    }`}
-                    onClick={() => setMenuIsVisible(false)}
-                  >
-                    Sobre nós
-                  </ul>
-                </Link>
-              </ScrollLink>
-              <Link to="/oftalmo">
-                <ul
-                  className={`${
-                    path === "/oftalmo" ? " color Options" : "Options"
-                  }`}
-                  onClick={() => setMenuIsVisible(false)}
-                >
-                  Serviços
-                </ul>
-              </Link>
-              <ScrollLink to="duvidas">
-                <Link to="/">
-                  <ul
-                    className={`${
-                      path === "/a" ? " color Options" : "Options"
-                    }`}
-                    onClick={() => setMenuIsVisible(false)}
-                  >
-                    Dúvidas
-                  </ul>
-                </Link>
-              </ScrollLink>
-              <a href="https://www.google.com/" target="blanked">
-                <ul
-                  className={`${path === "/a" ? " color Options" : "Options"}`}
-                  onClick={() => setMenuIsVisible(false)}
-                >
-                  Atendimento
-                </ul>
-              </a>
-              <ScrollLink to="parceiros">
-                <Link to="/">
-                  <ul
-                    className={`${
-                      path === "/a" ? " color Options" : "Options"
-                    }`}
-                    onClick={() => setMenuIsVisible(false)}
-                  >
-                    Parceiros
-                  </ul>
-                </Link>
-              </ScrollLink>
-            </li>
-          </div>
-        </nav>
-      </HeaderContainer>
-    </>
+    <HeaderContainer isVisible={menuIsVisible} isanimation={animation}>
+      <nav className="header">
+        <div className="content">
+          <Link to="/">
+            <div className="logoSide">
+              <img src={logo} alt="logotipo" />
+            </div>
+          </Link>
+          <AiOutlineMenu
+            onClick={() => setMenuIsVisible(true)}
+            className="menu"
+          />
+          <AiOutlineClose
+            onClick={handleMenuClose}
+            className={`menu closeMenu ${menuIsVisible ? "visible" : ""}`}
+          />
+          <ul className="menuSide">
+            {menuItems.map((item) => (
+              <li key={item.path} className="Options  ">
+                {item.path === wppLink ? (
+                  <a href={item.path} target="_blank" rel="noopener noreferrer">
+                    {item.label}
+                  </a>
+                ) : item.path.startsWith("/") ? (
+                  <Link to={item.path}>{item.label}</Link>
+                ) : path === "/oftalmo" ? (
+                  <Link to="/">{item.label}</Link>
+                ) : (
+                  <ScrollLink to={item.path} onClick={handleMenuClose}>
+                    {item.label}
+                  </ScrollLink>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </HeaderContainer>
   );
 }

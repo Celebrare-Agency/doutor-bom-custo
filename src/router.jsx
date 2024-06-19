0c8a5524ee6188c93a6f383b3e9e104b403bc8de
Tiago Santos <tiagoalmeidasantos04@gmail.comDD>
2024-06-19 14:01:17 -0300

    commit

--- src/router.jsx
+++ src/router.jsx
@@ -3,27 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Loading from "./components/GLOBAL/loading/index";
 
 const Home = lazy(() => import("./pages/1.home/index"));
-const OphthalmologistParaisopolisMedicoDosolhos = lazy(() =>
-  import("./pages/consultaLpSellV1 - Paraisopolis  Medico dos olhos/index")
-);
-const Oftalmo = lazy(() => import("./pages/consultaLpSellV1/index"));
+
 const BlefaroplastiaLpSell = lazy(() =>
   import("./pages/blefaroplastiaLpSell/index")
 );
 const Politica = lazy(() => import("./pages/politica/index"));
-const CatarataTks = lazy(() => import("./pages/obrigado-catarata/index"));
-const OphthalmologistGuarulhos = lazy(() =>
-  import("./pages/consultaLpSellV1 - Guarulhos/index")
-);
-const OphthalmologistParaisopolis = lazy(() =>
-  import("./pages/consultaLpSellV1 - Paraisopolis/index")
-);
-const Seller2 = lazy(() => import("./pages/consultaLpSellV2/index"));
+
 const Catarata = lazy(() => import("./pages/Blog/catarata/index"));
 const CatarataLpSell = lazy(() => import("./pages/catarataLpSell/index"));
-const GuarulhosSeller2 = lazy(() =>
-  import("./pages/consultaLpSellV2 - Guarulhos/index")
-);
 
 export function Router() {
   return (
@@ -33,24 +20,8 @@ export function Router() {
         <Route path="/blefaroplastia" element={<BlefaroplastiaLpSell />} />
         <Route path="/blog-Catarata" element={<Catarata />} />
         <Route path="/Catarata" element={<CatarataLpSell />} />
-        <Route path="/obrigado-catarata" element={<CatarataTks />} />
         <Route path="/politica" element={<Politica />} />
-        <Route
-          path="/medico-dos-olhos"
-          element={<OphthalmologistParaisopolisMedicoDosolhos />}
-        />
         <Route path="/" element={<Home />} />
-        <Route path="/oftalmo-taboaodaserra-v2" element={<Seller2 />} />
-        <Route path="/oftalmo-guarulhos-v2" element={<GuarulhosSeller2 />} />
-        <Route path="/oftalmo-taboaodaserra" element={<Oftalmo />} />
-        <Route
-          path="/oftalmo-guarulhos"
-          element={<OphthalmologistGuarulhos />}
-        />{" "}
-        <Route
-          path="/oftalmo-paraisopolis"
-          element={<OphthalmologistParaisopolis />}
-        />
       </Routes>
     </Suspense>
   );

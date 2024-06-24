8bd4fe796637ba9fd969a4c50b7e3486d8e96fcc
Tiago Santos <tiagoalmeidasantos04@gmail.comDD>
2024-06-24 09:06:47 -0300

    cp,o

--- src/pages/blefaroplastiaLpSell/index.jsx
+++ src/pages/blefaroplastiaLpSell/index.jsx
@@ -5,7 +5,6 @@ import Contact from "../../components/blefaroplastiaLpSell/2.callUs/index";
 import Price from "../../components/blefaroplastiaLpSell/3.price";
 import Depoiments from "../../components/blefaroplastiaLpSell/4.depoiments";
 import Location from "../../components/blefaroplastiaLpSell/5.location/index";
-import ModalComponent from "../../components/GLOBAL/modal-ads-catarata-urgencia/index";
 import WhatIs from "../../components/blefaroplastiaLpSell/6.whatIsBlefaroplastia";
 // import PromotionModal from "../../components/GLOBAL/Regressive -ads - catarata - count";
 
@@ -19,7 +18,7 @@ export default function CatarataLpSell() {
       <Contact />
       <Price />
       <Depoiments />
-      <Location /> <ModalComponent />
+      <Location />
     </>
   );
 }

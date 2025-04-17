df028e44045ce94f46421cfbdb08f3c89b2d5fc9
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2025-04-17 15:33:06 -0300

    commit

--- src/components/GLOBAL/footer/index.jsx
+++ src/components/GLOBAL/footer/index.jsx
@@ -35,12 +35,12 @@ export default function Footer() {
           </a>
 
           <a
-            href="https://api.whatsapp.com/send?phone=5511950212678&text=Ol%C3%A1!%20Eu%20gostaria%20de%20agendar%20uma%20consulta%20com%20Oftalmologista.%20"
+            href="https://api.whatsapp.com/send?phone=55970409738&text=Ol%C3%A1!%20Eu%20gostaria%20de%20agendar%20uma%20consulta%20com%20Oftalmologista.%20"
             target="blanked"
           >
             <div className="socialContainer row">
               <AiOutlineWhatsApp className="icon" />
-              <p>11 95021-2678</p>
+              <p> 97040-9738</p>
             </div>
           </a>
 
